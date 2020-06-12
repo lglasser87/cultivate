15013351c4f99d3cf6d52dec54326cab125fbe7c
Amy Fabella <amyfabella@hotmail.com>
2020-06-11 21:32:53 -0700

    fixed fontawesome imports

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import Button from 'react-bootstrap/Button';
 
 // This file exports the Input, TextArea, and FormBtn components
