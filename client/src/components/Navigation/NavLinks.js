59164211ede75a7919cc56c4cd6ec30d78e7c6f8
Amy Fabella <amyfabella@hotmail.com>
2020-06-11 21:34:37 -0700

    updated fontawesome import

--- client/src/components/Navigation/NavLinks.js
+++ client/src/components/Navigation/NavLinks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function NavLinks(props) {
     return <ul>
