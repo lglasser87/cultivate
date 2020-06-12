87fb0ed96f90ca386b03fc21f3ce4b119c0f728e
Amy Fabella <amyfabella@hotmail.com>
2020-06-11 19:29:36 -0700

    added bootstrap, font awesome, and css files

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
 
 // This file exports the Input, TextArea, and FormBtn components
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMoneyBill } from '@fortawesome/pro-solid-svg-icons';
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faMoneyBill,
+  faCode,
+  faHighlighter
+  // more icons go here
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
