eede9524f893c7a43dd18d552ddef2d3afeda8db
Amy Fabella <amyfabella@hotmail.com>
2020-06-11 22:36:00 -0700

    bootstrap navbar

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { 
     faSearch,
