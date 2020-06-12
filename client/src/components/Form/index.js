5d268e47f3816a075aad9a056a3b672e000760fd
Amy Fabella <amyfabella@hotmail.com>
2020-06-12 12:33:48 -0700

    replaced logo (Images/ cultivate.png and NavLinks.js) and updated Form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./style.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-// This file exports the Input, TextArea, and FormBtn components
+// This file exports the FormEl, Input, TextArea, and FormBtn components
+export function Form(props) {
+  return (
+    <Grid className="center aligned">
+      <div className="form-group" class="form">
+        <form class="form" {...props}>
+        </form>
+      </div>
+    </Grid>
+  );
+}
 
 export function Input(props) {
   return (
@@ -29,3 +39,55 @@ export function FormBtn(props) {
     </button>
   );
 }
+//=========================================================
+//BOOTSTRAP FORM
+//=========================================================
+
+// import React, {Component} from "react";
+// import { Form, Button } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import "./NavLinks.css";
+
+// class FormEl extends Component {
+//   render() {
+//     return (
+//       <Form></Form>
+//     )
+//   }
+// }
+
+// class Input extends Component {
+//   render() {
+//     return (
+//       <Form.Group controlId="formBasicEmail">
+//       <Form.Label>Email address</Form.Label>
+//       <Form.Control type="email" placeholder="Enter email" />
+//     </Form.Group>
+//     )
+//   }
+// }
+
+// class TextArea extends Component {
+//   render() {
+//     return (
+//       <Form.Group controlId="exampleForm.ControlTextarea1">
+//       <Form.Label>Example textarea</Form.Label>
+//       <Form.Control as="textarea" rows="3" />
+//     </Form.Group>
+//     )
+//   }
+// }
+// class FormBtn extends Component {
+//   render() {
+//     return (
+//       <Button variant="primary" type="submit">
+//       Submit
+//     </Button>
+//     )
+//   }
+// }
+
+//export default FormEl;
+//export default Input;
+//export default TextArea;
+//export default FormBtn;
--- client/src/pages/Authentication.js
+++ client/src/pages/Authentication.js
@@ -1,26 +1,26 @@
 import React from "react";
-
-import { Container, Row, Col } from "../components/Grid"
-import { Input, FormBtn} from "../components/Form";
+import { Container, Row, Col } from "../components/Grid";
+import { Form, Input, FormBtn} from "../components/Form";
+// import { FormEl, Input, FormBtn} from "../components/Form"; if using Bootstrap form
 
 function Authentication() {
     return (
     <Container fluid>
         <Row>
             <Col size="md-4">
-                <form>
-                <Input
-                    name="userName"
-                    placeholder="User Name"
-                />
-                <Input
-                    name="password"
-                    placeholder="Password"
-                />
-                <FormBtn>
-                    Login
-                </FormBtn>
-                </form>
+                <Form>
+                    <Input
+                        name="userName"
+                        placeholder="User Name"
+                    />
+                    <Input
+                        name="password"
+                        placeholder="Password"
+                    />
+                    <FormBtn>
+                        Login
+                    </FormBtn>
+                </Form>
             </Col>
         </Row>
     </Container>
