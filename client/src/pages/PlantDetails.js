3c0d22a2e1a27471680ace22ca839f9ebc9c4b4d
Player 1 <j.banuelos00@gmail.com>
2020-06-12 22:47:43 -0700

    finalized framework for plants and plantsdetail page.

--- client/src/pages/PlantDetails.js
+++ client/src/pages/PlantDetails.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import { Col, Row, Container } from "../components/Grid"
 import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API"
 
-function PlantDetails() {
+function PlantDetails(props) {
+    const [plant, setPlant] = useState({})
+
+    // When this component mounts, grab the book with the _id of props.match.params.id
+    // e.g. localhost:3000/plants/599dcb67f0f16317844583fc
+    const {id} = useParams()
+    useEffect(() => {
+        API.getPlant(id)
+            .then(res => setPlant(res.data))
+            .catch(err => console.log(err));
+    }, [])
+
     return (
         <Container fluid>
             <Row>
                 <Col size="md-12">
                    <Jumbotron>
                        <h1>
-                           Plant Name Here
+                           {plant.name}
                        </h1>
                    </Jumbotron>
                 </Col>
@@ -21,7 +33,22 @@ function PlantDetails() {
                    <article>
                        <h1>Description</h1>
                        <p>
-                           Plant Description here
+                           {plant.description}
+                       </p>
+                       <br />
+                       <h1>Best Season</h1>
+                       <p>
+                           {plant.season}
+                       </p>
+                       <br />
+                       <h1>Getting Started</h1>
+                       <p>
+                           {plant.gettingStarted}
+                       </p>
+                       <br />
+                       <h1>How to Maintain your Plant</h1>
+                       <p>
+                           {plant.howToMaintain}
                        </p>
                    </article>
                 </Col>
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -8,18 +8,18 @@ mongoose.connect(
 
 const plantSeed = [
     {
-        name: "",
-        description: "",
-        season: "",
-        gettingStarted: "",
-        howToMaintain: "",
-        rotate: ""
+        name: "Test",
+        description: "test",
+        season: "test",
+        gettingStarted: "test",
+        howToMaintain: "test",
+        rotate: "test"
     }
 ]
 
 db.Plant
   .remove({})
-  .then(() => db.Book.collection.insertMany(plantSeed))
+  .then(() => db.Plant.collection.insertMany(plantSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
